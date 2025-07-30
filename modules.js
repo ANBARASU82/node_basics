7a17fc8de3008f9a894014aef4cd233b9a25d143
ANBARASU82 <anbarasukolanchinathan@gmail.com>
2025-07-30 14:41:05 +0530

    first coomit

---
+++ modules.js
@@ -0,0 +1,45 @@
+const xyz=require('./sub.js')
+const os=require('os');
+console.log(xyz.rcb);
+console.log(os.platform(),os.homedir());
+
+const fs = require('fs');
+
+
+fs.appendFile('./docs/ab.text','\n Abdevilliers is the blood of RCB',(err)=>{
+    if(err){
+        console.log(err);
+    }
+})
+fs.readFile('./docs/ab.text',{encoding:'utf8'},(err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log(data);
+})
+
+if (! fs.existsSync('./ass')){
+    fs.mkdir('./ass',(err)=>{
+        if(err){
+            console.log(err);
+        }
+        console.log("file created");
+    })
+}
+else{
+    fs.rmdir('./ass',(err)=>{
+        if(err){
+            console.log(err);
+        }
+    })
+}
+
+const readstream = fs.createReadStream('./docs/ab.text',{encoding:'utf8'});
+const writestream = fs.createWriteStream('./docs/ab1.text');
+readstream.on('data',(chunk)=>{
+    console.log(chunk);
+    writestream.write("\n new book \n");
+    writestream.write(chunk);
+});
+
+readstream.pipe(writestream);
---
+++ sub.js
@@ -0,0 +1,4 @@
+const cricket =['virat','abdevilliers','siraj']
+console.log(cricket);
+const rcb_prime=['2016,2025']
+module.exports ={cricket:cricket,rcb:rcb_prime};
