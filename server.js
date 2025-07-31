d1374ea2e06a8c73c5282e169b33323bd52f550a
ANBARASU82 <anbarasukolanchinathan@gmail.com>
2025-07-31 14:11:27 +0530

    first

---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req,res)=>{
+    path='./views/';
+    res.setHeader('content-type','text/html');
+    switch(req.url){
+        case '/index':
+            path += 'index.html';
+            res.statusCode=200;
+            break;
+        case '/about':
+            path+='about.html';
+            res.statusCode=200;
+            break;
+        case '/about-me':
+            res.statusCode=301;
+            res.setHeader('Location','/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+    fs.readFile(path,(err,data)=>{
+        if(err){
+            console.log(err);
+            res.statusCode = 500;
+            res.end();
+        }
+        else{
+            res.write(data);
+            res.end();
+        }
+        
+    })
+});
+server.listen(3000,'localhost',()=>{
+    console.log('Server is running on port 3000');
+});
